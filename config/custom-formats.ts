fdaa6666c5a40788112ce6b36bc8e9c1b16f3ee4
Niadevv <niadel@virginmedia.com>
2020-12-22 16:42:59 +0000

    Implemented signature z moves only clause

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -4,15 +4,30 @@ export const Formats: FormatList = [
 	},
 	{
 		name: "[Gen 8] Heat Enteis Ubers",
-		desc: "Assorted buffs and eventually new megas",
+		desc: "Assorted buffs and new megas",
 		mod: 'heatenteisformat',
-		// ruleset: ['[Gen 8] National Dex AG'],
 		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
+		onValidateSet(set) {
+			const item = this.dex.getItem(set.item);
+			if (item.zMove && !['ultranecroziumz', 'aloraichiumz', 'decidiumz', 'eeviumz', 'kommoniumz', 'lunaliumz',
+			 'lycaniumz', 'marshadiumz', 'mewniumz', 'mimikiumz', 'pikaniumz', 'pikashuniumz', 'primariumz', 'snorliumz',
+			 'solganiumz', 'tapuniumz'].includes(item.id)) {
+				return [`${set.name || set.species}'s item ${item.name} is banned by Signature Z-Moves Only Clause.`];
+			}
+		},
 	},
 	{
 		name: "[Gen 8] Heat Enteis",
-		desc: "Assorted buffs and eventually new megas",
+		desc: "Assorted buffs and new megas",
 		mod: 'heatenteisformat',
 		ruleset: ['[Gen 8] National Dex'],
+		onValidateSet(set) {
+			const item = this.dex.getItem(set.item);
+			if (item.zMove && !['ultranecroziumz', 'aloraichiumz', 'decidiumz', 'eeviumz', 'kommoniumz', 'lunaliumz',
+			 'lycaniumz', 'marshadiumz', 'mewniumz', 'mimikiumz', 'pikaniumz', 'pikashuniumz', 'primariumz', 'snorliumz',
+			 'solganiumz', 'tapuniumz'].includes(item.id)) {
+				return [`${set.name || set.species}'s item ${item.name} is banned by Signature Z-Moves Only Clause.`];
+			}
+		},
 	},
 ];
---
+++ data/mods/heatenteisformat/rulesets.ts
@@ -0,0 +1,15 @@
+export const Formats: {[k: string]: ModdedFormatData} = {
+	sigzmovesonly: {
+		effectType: 'ValidatorRule',
+		name: "Signature Z-Moves Only",
+		desc: "Bans all non Signature Z moves.",
+		onValidateSet(set, format) {
+			const item = this.dex.getItem(set.item);
+			if (item.zMove && !['ultranecroziumz', 'aloraichiumz', 'decidiumz', 'eeviumz', 'kommoniumz', 'lunaliumz',
+			 'lycaniumz', 'marshadiumz', 'mewniumz', 'mimikiumz', 'pikaniumz', 'pikashuniumz', 'primariumz', 'snorliumz',
+			 'solganiumz', 'tapuniumz'].includes(item.id)) {
+				return [`${set.name || set.species}'s item ${item.name} is banned by Signature Z-Moves Only Clause.`];
+			}
+		},
+	},
+};
