ba319646101fb37dda59bae9b889561be1f8278b
Niadevv <niadel@virginmedia.com>
2020-12-22 23:45:51 +0000

    Now this world shall know pain! [Freed everything bar Baton Pass and Moody]

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -20,7 +20,14 @@ export const Formats: FormatList = [
 		name: "[Gen 8] Heat Enteis",
 		desc: "Assorted buffs and new megas",
 		mod: 'heatenteisformat',
-		ruleset: ['[Gen 8] National Dex'],
+		// ruleset: ['[Gen 8] National Dex'],
+		ruleset: ['OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
+		banlist: [
+			'Arceus', 'Calyrex-Ice', 'Calyrex-Shadow', 'Dialga', 'Eternatus', 'Giratina', 'Giratina-Origin', 'Groudon',
+			'Ho-Oh', 'Kyogre', 'Kyurem-Black', 'Kyurem-White', 'Lugia', 'Lunala', 'Marshadow', 'Mewtwo', 'Necrozma-Dawn-Wings',
+			'Necrozma-Dusk-Mane', 'Necrozma-Ultra', 'Palkia', 'Rayquaza', 'Reshiram', 'Solgaleo', 'Xerneas', 'Yveltal',
+			'Zacian', 'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned', 'Zekrom', 'Baton Pass',
+		],
 		onValidateSet(set) {
 			const item = this.dex.getItem(set.item);
 			if (item.zMove && !['ultranecroziumz', 'aloraichiumz', 'decidiumz', 'eeviumz', 'kommoniumz', 'lunaliumz',
