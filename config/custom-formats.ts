bf57fae42e0cd4860c80155623e08fbf80cc2f65
Niadevv <niadel@virginmedia.com>
2021-01-09 16:36:52 +0000

    Add OUBL and UU tiers, actually banned Regigigas-Origin.

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -1,6 +1,6 @@
 export const Formats: FormatList = [
 	{
-		section: "Pet Mods",
+		section: "Sw/Sh Singles",
 	},
 	{
 		name: "[Gen 8] Heat Enteis OU",
@@ -10,7 +10,7 @@ export const Formats: FormatList = [
 		banlist: [
 			'Arceus', 'Arceus-Origin', 'Calyrex-Shadow', 'Dialga', 'Eternatus', 'Eternatus-Eternamax', 'Giratina', 'Giratina-Origin', 'Groudon',
 			'Ho-Oh', 'Kyogre', 'Kyurem-Black', 'Kyurem-White', 'Lugia', 'Lunala', 'Mewtwo', 'Necrozma-Dawn-Wings',
-			'Necrozma-Dusk-Mane', 'Necrozma-Ultra', 'Palkia', 'Rayquaza', 'Reshiram', 'Solgaleo', 'Xerneas', 'Yveltal',
+			'Necrozma-Dusk-Mane', 'Necrozma-Ultra', 'Palkia', 'Rayquaza', 'Regigigas-Origin', 'Reshiram', 'Solgaleo', 'Xerneas', 'Yveltal',
 			'Zacian', 'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned', 'Zekrom', 'Baton Pass',
 		],
 		onValidateSet(set) {
@@ -63,6 +63,83 @@ export const Formats: FormatList = [
 				if (eternamax > 1) return ["You can only use one Eternatus-Eternamax."];
 			}
 		},
+	},
+	{
+		name: "[Gen 8] Heat Enteis UU",
+		desc: "Assorted buffs and new megas",
+		mod: 'heatenteisformat',
+		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Overflow Stat Mod'],
+		banlist: [
+			'Arceus', 'Arceus-Origin', 'Calyrex-Shadow', 'Dialga', 'Eternatus', 'Eternatus-Eternamax', 'Giratina', 'Giratina-Origin', 'Groudon',
+			'Ho-Oh', 'Kyogre', 'Kyurem-Black', 'Kyurem-White', 'Lugia', 'Lunala', 'Mewtwo', 'Necrozma-Dawn-Wings',
+			'Necrozma-Dusk-Mane', 'Necrozma-Ultra', 'Palkia', 'Rayquaza', 'Regigigas-Origin', 'Reshiram', 'Solgaleo', 'Xerneas', 'Yveltal',
+			'Zacian', 'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned', 'Zekrom', 'Baton Pass',
+			// OU
+			'Alakazam-Mega', 'Blastoise-Mega', 'Blissey', 'Blaziken', 'Blaziken-Mega', 'Calyrex-Ice', 'Chansey', 'Charizard-Mega-X', 'Charizard-Mega-Y',
+			'Cinderace', 'Clefable', 'Corviknight', 'Darkrai', 'Darmanitan-Galar', 'Darmanitan-Galar-Zen', 'Excadrill', 'Ferrothorn', 'Floette-Eternal',
+			'Garchomp', 'Genesect', 'Genesect-Burn', 'Genesect-Douse', 'Genesect-Chill', 'Genesect-Shock', 'Gengar-Mega', 'Gliscor', 'Greninja', 'Greninja-Ash',
+			'Heatran', 'Kartana', 'Lopunny-Mega', 'Lucario-Mega', 'Magearna', 'Magnezone', 'Marshadow', 'Mawile-Mega', 'Medicham-Mega', 'Metagross-Mega',
+			'Naganadel', 'Pelipper', 'Pheromosa', 'Rillaboom', 'Salamence-Mega', 'Scizor-Mega', 'Serperior', 'Tangrowth', 'Tapu Fini', 'Tapu Koko', 'Tapu Lele',
+			'Tornadus-Therian', 'Toxapex', 'Tyranitar', 'Urshifu-Single-Strike', 'Volcarona', 'Zapdos', 'Zygarde',
+			// OU by technicality
+			'Garchomp-Mega', 'Tyranitar-Mega',
+		],
+		onValidateSet(set) {
+			const item = this.dex.getItem(set.item);
+			if (item.zMove && !['ultranecroziumz', 'aloraichiumz', 'decidiumz', 'eeviumz', 'kommoniumz', 'lunaliumz',
+			 'lycaniumz', 'marshadiumz', 'mewniumz', 'mimikiumz', 'pikaniumz', 'pikashuniumz', 'primariumz', 'snorliumz',
+			 'solganiumz', 'tapuniumz', 'simisagiumz', 'simipouriumz', 'simiseariumz'].includes(item.id)) {
+				return [`${set.name || set.species}'s item ${item.name} is banned by Signature Z-Moves Only Clause.`];
+			}
+		},
+		onValidateTeam(team) {
+			let originceus = 0;
+			let origigas = 0;
+			let eternamax = 0;
+			for (const set of team) {
+				if (set.species === 'Arceus-Origin') originceus++;
+				if (set.species === 'Regigigas-Origin') origigas++;
+				if (set.species === 'Eternatus-Eternamax') eternamax++;
+				if (originceus > 1) return ["You can only use one Arceus-Origin."];
+				if (origigas > 1) return ["You can only use one Regigigas-Origin."];
+				if (eternamax > 1) return ["You can only use one Eternatus-Eternamax."];
+			}
+		},
+		// debug: true, // TEMP, REMOVEME
+	},
+	{
+		name: "[Gen 8] Heat Enteis OUBL",
+		desc: "Assorted buffs and new megas",
+		mod: 'heatenteisformat',
+		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Overflow Stat Mod'],
+		banlist: [
+			'Arceus', 'Arceus-Origin', 'Calyrex-Shadow', 'Dialga', 'Eternatus', 'Eternatus-Eternamax', 'Giratina', 'Giratina-Origin', 'Groudon',
+			'Ho-Oh', 'Kyogre', 'Kyurem-Black', 'Kyurem-White', 'Lugia', 'Lunala', 'Mewtwo', 'Necrozma-Dawn-Wings',
+			'Necrozma-Dusk-Mane', 'Necrozma-Ultra', 'Palkia', 'Rayquaza', 'Reshiram', 'Solgaleo', 'Xerneas', 'Yveltal',
+			'Zacian', 'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned', 'Zekrom', 'Baton Pass',
+		],
+		onValidateSet(set) {
+			const item = this.dex.getItem(set.item);
+			if (item.zMove && !['ultranecroziumz', 'aloraichiumz', 'decidiumz', 'eeviumz', 'kommoniumz', 'lunaliumz',
+			 'lycaniumz', 'marshadiumz', 'mewniumz', 'mimikiumz', 'pikaniumz', 'pikashuniumz', 'primariumz', 'snorliumz',
+			 'solganiumz', 'tapuniumz', 'simisagiumz', 'simipouriumz', 'simiseariumz'].includes(item.id)) {
+				return [`${set.name || set.species}'s item ${item.name} is banned by Signature Z-Moves Only Clause.`];
+			}
+		},
+		onValidateTeam(team) {
+			let originceus = 0;
+			let origigas = 0;
+			let eternamax = 0;
+			for (const set of team) {
+				if (set.species === 'Arceus-Origin') originceus++;
+				if (set.species === 'Regigigas-Origin') origigas++;
+				if (set.species === 'Eternatus-Eternamax') eternamax++;
+				if (originceus > 1) return ["You can only use one Arceus-Origin."];
+				if (origigas > 1) return ["You can only use one Regigigas-Origin."];
+				if (eternamax > 1) return ["You can only use one Eternatus-Eternamax."];
+			}
+		},
+		// debug: true, // TEMP, REMOVEME
 	},
 	{
 		name: "[Gen 8] Heat Enteis Anything Goes",
