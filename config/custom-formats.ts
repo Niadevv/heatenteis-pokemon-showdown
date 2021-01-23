052f8210badde5d22df359d14474f595bbd82914
Niadevv <niadel@virginmedia.com>
2021-01-23 01:28:18 +0000

    Accidentally freed Baton Pass in Ubers whoooppsss.

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -43,7 +43,7 @@ export const Formats: FormatList = [
 		desc: "Assorted buffs and new megas",
 		mod: 'heatenteisformat',
 		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Overflow Stat Mod'],
-		banlist: ["Arceus-Origin", "Eternatus-Eternamax"],
+		banlist: ["Arceus-Origin", "Eternatus-Eternamax", "Baton Pass"],
 		onValidateSet(set) {
 			const item = this.dex.getItem(set.item);
 			if (item.zMove && !['ultranecroziumz', 'aloraichiumz', 'decidiumz', 'eeviumz', 'kommoniumz', 'lunaliumz',
